6b5bd86193b08cc789be4f56b6fed61d2dedc2ad
swathi-sk <swathi.sashikumar7@gmail.com>
2017-06-05 18:56:21 +0530

    work

---
+++ js/gui.js
@@ -0,0 +1,246 @@
+$("#SetFen").click(function () {
+	var fenStr = $("#fenIn").val();	
+	NewGame(fenStr);
+});
+
+function NewGame(fenStr) {
+	ParseFen(fenStr);
+	PrintBoard();
+	SetInitialBoardPieces();
+}
+
+function ClearAllPieces() {
+	$(".Piece").remove();
+}
+
+function SetInitialBoardPieces() {
+
+	var sq;
+	var sq120;
+	var file,rank;
+	var rankName;
+	var fileName;
+	var imageString;
+	var pieceFileName;
+	var pce;
+	
+	ClearAllPieces();
+	
+	for(sq = 0; sq < 64; ++sq) {
+		sq120 = SQ120(sq);
+		pce = GameBoard.pieces[sq120];
+		if(pce >= PIECES.wP && pce <= PIECES.bK) {
+			AddGUIPiece(sq120, pce);
+		}
+	}
+}
+
+function DeSelectSq(sq) {
+	$('.Square').each( function(index) {
+		if(PieceIsOnSq(sq, $(this).position().top, $(this).position().left) == BOOL.TRUE) {
+				$(this).removeClass('SqSelected');
+		}
+	} );
+}
+
+function SetSqSelected(sq) {
+	$('.Square').each( function(index) {
+		if(PieceIsOnSq(sq, $(this).position().top, $(this).position().left) == BOOL.TRUE) {
+				$(this).addClass('SqSelected');
+		}
+	} );
+}
+
+function ClickedSquare(pageX, pageY) {
+	console.log('ClickedSquare() at ' + pageX + ',' + pageY);
+	var position = $('#Board').position();
+	
+	var workedX = Math.floor(position.left);
+	var workedY = Math.floor(position.top);
+	
+	pageX = Math.floor(pageX);
+	pageY = Math.floor(pageY);
+	
+	var file = Math.floor((pageX-workedX) / 60);
+	var rank = 7 - Math.floor((pageY-workedY) / 60);
+	
+	var sq = FR2SQ(file,rank);
+	
+	console.log('Clicked sq:' + PrSq(sq));
+	
+	SetSqSelected(sq);	
+	
+	return sq;
+}
+
+$(document).on('click','.Piece', function (e) {
+	console.log('Piece Click');
+	
+	if(UserMove.from == SQUARES.NO_SQ) {
+		UserMove.from = ClickedSquare(e.pageX, e.pageY);
+	} else {
+		UserMove.to = ClickedSquare(e.pageX, e.pageY);
+	}
+	
+	MakeUserMove();
+	
+});
+
+$(document).on('click','.Square', function (e) {
+	console.log('Square Click');	
+	if(UserMove.from != SQUARES.NO_SQ) {
+		UserMove.to = ClickedSquare(e.pageX, e.pageY);
+		MakeUserMove();
+	}
+
+});
+
+function MakeUserMove() {
+
+	if(UserMove.from != SQUARES.NO_SQ && UserMove.to != SQUARES.NO_SQ) {
+	
+		console.log("User Move:" + PrSq(UserMove.from) + PrSq(UserMove.to));	
+		
+		var parsed = ParseMove(UserMove.from,UserMove.to);
+		
+		if(parsed != NOMOVE) {
+			MakeMove(parsed);
+			PrintBoard();
+			MoveGUIPiece(parsed);
+		}
+	
+		DeSelectSq(UserMove.from);
+		DeSelectSq(UserMove.to);
+		
+		UserMove.from = SQUARES.NO_SQ;
+		UserMove.to = SQUARES.NO_SQ;
+	}
+
+}
+
+function PieceIsOnSq(sq, top, left) {
+
+	if( (RanksBrd[sq] == 7 - Math.round(top/60) ) && 
+		FilesBrd[sq] == Math.round(left/60) ) {
+		return BOOL.TRUE;
+	}
+		
+	return BOOL.FALSE;
+
+}
+
+function RemoveGUIPiece(sq) {
+
+	$('.Piece').each( function(index) {
+		if(PieceIsOnSq(sq, $(this).position().top, $(this).position().left) == BOOL.TRUE) {
+			$(this).remove();
+		}
+	} );
+	
+}
+
+function AddGUIPiece(sq, pce) {
+
+	var file = FilesBrd[sq];
+	var rank = RanksBrd[sq];
+	var rankName = "rank" + (rank+1);
+	var	fileName = "file" + (file+1);
+	var pieceFileName = "images/" + SideChar[PieceCol[pce]] + PceChar[pce].toUpperCase() + ".png";
+	var	imageString = "<image src=\"" + pieceFileName + "\" class=\"Piece " + rankName + " " + fileName + "\"/>";
+	$("#Board").append(imageString);
+}
+
+function MoveGUIPiece(move) {
+	
+	var from = FROMSQ(move);
+	var to = TOSQ(move);	
+	
+	if(move & MFLAGEP) {
+		var epRemove;
+		if(GameBoard.side == COLOURS.BLACK) {
+			epRemove = to - 10;
+		} else {
+			epRemove = to + 10;
+		}
+		RemoveGUIPiece(epRemove);
+	} else if(CAPTURED(move)) {
+		RemoveGUIPiece(to);
+	}
+	
+	var file = FilesBrd[to];
+	var rank = RanksBrd[to];
+	var rankName = "rank" + (rank+1);
+	var	fileName = "file" + (file+1);
+	
+	$('.Piece').each( function(index) {
+		if(PieceIsOnSq(from, $(this).position().top, $(this).position().left) == BOOL.TRUE) {
+			$(this).removeClass();
+			$(this).addClass("Piece " + rankName + " " + fileName);
+		}
+	} );
+	
+	if(move & MFLAGCA) {
+		switch(to) {
+			case SQUARES.G1: RemoveGUIPiece(SQUARES.H1); AddGUIPiece(SQUARES.F1, PIECES.wR); break;
+			case SQUARES.C1: RemoveGUIPiece(SQUARES.A1); AddGUIPiece(SQUARES.D1, PIECES.wR); break;
+			case SQUARES.G8: RemoveGUIPiece(SQUARES.H8); AddGUIPiece(SQUARES.F8, PIECES.bR); break;
+			case SQUARES.C8: RemoveGUIPiece(SQUARES.A8); AddGUIPiece(SQUARES.D8, PIECES.bR); break;
+		}
+	} else if (PROMOTED(move)) {
+		RemoveGUIPiece(to);
+		AddGUIPiece(to, PROMOTED(move));
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
